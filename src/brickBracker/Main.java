80ab3f729a619a1c7cf69aeeb213adae1c661147
GuilhermeGDGP <guilhermedgoncalvez@gmail.com>
2020-10-06 18:38:39 -0300

    Implementando a classe Gameplay e movimentos da plataforma

--- src/brickBracker/Main.java
+++ src/brickBracker/Main.java
@@ -7,12 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame jf = new JFrame();
+        Gameplay gameplay = new Gameplay();
 
         jf.setBounds(10, 10, 700, 600);
         jf.setTitle("Breakout Ball");
         jf.setResizable(false);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jf.add(gameplay);
 
     }
 }
